953b7ad09c65a713bffa15f2804d7f39a0f35335
João Nunes <lockenunes@gmail.com>
2018-05-19 08:36:17 -0300

    Nice work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('.btn-do-something').on('click', function() {
-		$('.jumbotron').slideUp(200, function() {
+		$('.jumbotrons').slideUp(200, function() {
 			$('.marketing').fadeOut(200, function() {
 				$('.jumbotron-part-two').fadeIn();
 			});
